1a37f8017040d83704eab6ed30b3037328054f22
Maximilian Dorninger <97409287+maxdorninger@users.noreply.github.com>
2026-05-12 09:12:16 +0200

    feat: add installation script for package setup and cleanup

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+URL="https://github.com/SZ-Laeuft/checkpoint/raw/refs/heads/master/pkg/checkpoint_0.1.0_arm64.deb"
+TMP_DEB="/tmp/checkpoint_0.1.0_arm64.deb"
+BASE_DIR="/home/checkpoint"
+
+echo "Removing app folder..."
+sudo rm -rf "$BASE_DIR/app"
+
+echo "Removing folders containing 'checkpoint' in their name..."
+find "$BASE_DIR" -maxdepth 1 -type d -name '*checkpoint*' ! -path "$BASE_DIR" -exec rm -rf {} +
+
+echo "Cleaning up..."
+rm -f "$TMP_DEB"
+
+echo "Downloading package..."
+curl -L "$URL" -o "$TMP_DEB"
+
+echo "Installing package..."
+sudo apt-get update
+sudo apt-get install -y "$TMP_DEB"
+
+
+echo "Done."
